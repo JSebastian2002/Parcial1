fd673de237bcfbcb904bc14103f0def93e4fc4c1
JSebastian2002 <JSebastian2002>
2022-11-10 17:50:41 -0500

    Parcial

---
+++ parcial/src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+// Codigo desviacion estandar escala 
+val n = List(727.7, 1086.5, 1091.0, 1361.3, 1490.5 )
+
+/*val desviacion = (n : Double) => (1 to n -1).forall( n % _!= 0)
+ n.map(desviacion(_)match{
+  case true => 1
+  case false => 0
+}).sum
+*/
+
+def pr(n)
+suma = 0
+
+def desviacion(nro: Double)={
+
+  val dsv = (n : Double) =>(1 to n)^2
+  val sm = 0
+  val media = 0
+  val varianza = 0.0
+
+sm = sm + n
+media = sumatoria / 5
+varianza = varianza / 5
+}
+
+
+
+
+
+
+
+
